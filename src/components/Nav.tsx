5514c99ce4dfc6ab260ee069513e43825e60a9ff
Lucas Moraes <lucasmo066@gmail.com>
2024-04-18 22:57:15 -0400

    mode toggle works, nav and hero set up

---
+++ src/components/Nav.tsx
@@ -0,0 +1,41 @@
+'use client'
+import Image from "next/image"
+import ModeToggle from "./ui/toggle-mode"
+
+export default function Nav() {
+  return (
+    <header>
+        <nav className="flex items-center justify-between w-full p-4">
+            <ul className="flex items-center space-x-4">
+            <li>
+                <a
+                    className="pointer-events-none flex items-center gap-2 p-8"
+                    href="/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        src="/vercel.svg"
+                        alt="Logo"
+                        className="dark:invert"
+                        width={100}
+                        height={24}
+                        priority
+                    />
+                </a>
+            </li>
+
+                <li>
+                    <a href="/" className="text-primary">Home</a>
+                </li>
+                <li>
+                    <a href="/about" className="text-blue-500">About</a>
+                </li>
+            <li>
+               <ModeToggle />
+            </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
